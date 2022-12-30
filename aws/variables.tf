088b86aedfdbfdc55572484deb00d25d68f92adc
Khaja Ehtesham <ehteshamkhaja@gmail.com>
2022-12-30 20:37:58 +0530

    adding variables

---
+++ aws/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc" {
+  default = "172.16.0.0/16"
+}
+variable "subnet" {
+  default = "172.16.10.0/24"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "region" {
+  default = "us-west-2"
+}
